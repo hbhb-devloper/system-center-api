95772d7ed0b22bff54579b45dbc93b78bdd5eae4
xiaokang <13148807065@163.com>
2020-10-16 02:52:28 +0800

    代码优化

--- src/main/java/com/hbhb/cw/systemcenter/api/SysDictApi.java
+++ src/main/java/com/hbhb/cw/systemcenter/api/SysDictApi.java
@@ -3,21 +3,16 @@ package com.hbhb.cw.systemcenter.api;
 import com.hbhb.cw.systemcenter.model.SysDict;
 import com.hbhb.cw.systemcenter.vo.SysDictResVO;
 import com.hbhb.cw.systemcenter.vo.SysDictVO;
-import com.hbhb.springboot.web.view.Page;
 
+import org.beetl.sql.core.page.PageResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-
 
 /**
  * @author xiaokang
@@ -25,43 +20,30 @@ import io.swagger.v3.oas.annotations.Parameter;
  */
 public interface SysDictApi {
 
-    @Operation(summary = "分页获取字典列表")
     @GetMapping("/list")
-    Page<SysDictResVO> getDictList(
-            @Parameter(description = "页码，默认为1") @RequestParam(required = false) Long pageNum,
-            @Parameter(description = "每页数量，默认为10") @RequestParam(required = false) Long pageSize,
-            @Parameter(description = "字典类型名称（模糊查询）") @RequestParam(required = false) String dictTypeName,
-            @Parameter(description = "字典标签（模糊查询）") @RequestParam(required = false) String dictLabel);
+    PageResult<SysDictResVO> getDictList(Long pageNum, Integer pageSize, String dictTypeName, String dictLabel);
 
-    @Operation(summary = "获取单条字典详情")
     @GetMapping("/info/{id}")
-    SysDict getDictInfo(@Parameter(description = "id") @PathVariable Integer id);
+    SysDict getDictInfo(@PathVariable Integer id);
 
-    @Operation(summary = "新增字典")
     @PostMapping("")
-    void saveDict(@Parameter(description = "字典实体") @RequestBody SysDict sysDict);
+    void saveDict(@RequestBody SysDict sysDict);
 
-    @Operation(summary = "更新字典")
     @PutMapping("")
-    void updateDict(@Parameter(description = "字典实体") @RequestBody SysDict sysDict);
+    void updateDict(@RequestBody SysDict sysDict);
 
-    @Operation(summary = "删除字典")
     @DeleteMapping("/{id}")
-    void deleteDict(@Parameter(description = "id") @PathVariable Integer id);
+    void deleteDict(@PathVariable Integer id);
 
-    @Operation(summary = "功能模块字典列表")
     @GetMapping("/module")
     List<SysDictVO> getModuleList();
 
-    @Operation(summary = "项目签报-流程状态")
     @GetMapping("/budget/project-status")
     List<SysDictVO> getProjectStatus();
 
-    @Operation(summary = "项目签报-项目来源")
     @GetMapping("/budget/project-origin")
     List<SysDictVO> getProjectOrigin();
 
-    @Operation(summary = "项目签报-增值税率")
     @GetMapping("/budget/project-vat-rate")
     List<SysDictVO> getProjectVatRate();
 }
--- src/main/java/com/hbhb/cw/systemcenter/api/SysUserApi.java
+++ src/main/java/com/hbhb/cw/systemcenter/api/SysUserApi.java
@@ -9,28 +9,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-
 /**
  * @author xiaokang
  * @since 2020-10-06
  */
 public interface SysUserApi {
 
-    @Operation(summary = "根据用户id获取用户的基本信息")
     @GetMapping("/{userId}")
-    SysUserInfo getUserById(@Schema(description = "用户id", required = true) @PathVariable Integer userId);
+    SysUserInfo getUserById(@PathVariable Integer userId);
 
-    @Operation(summary = "根据登录名获取用户详情")
     @GetMapping("/info")
-    SysUser getUserByName(@Schema(description = "登录名", required = true) @RequestParam String userName);
+    SysUser getUserByName(@RequestParam String userName);
 
-    @Operation(summary = "获取用户所有角色")
     @GetMapping("/{userId}/roles")
-    List<Integer> getUserRoles(@Schema(description = "用户id", required = true) @PathVariable Integer userId);
+    List<Integer> getUserRoles(@PathVariable Integer userId);
 
-    @Operation(summary = "获取用户所有权限")
     @GetMapping("/{userId}/permissions")
-    List<String> getUserPerms(@Schema(description = "用户id", required = true) @PathVariable Integer userId);
+    List<String> getUserPerms(@PathVariable Integer userId);
 }
