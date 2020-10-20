90ab556bf28611b4c29592f66405c87ec47623d4
xiaokang <13148807065@163.com>
2020-10-20 18:48:20 +0800

    修改

--- src/main/java/com/hbhb/cw/systemcenter/model/SysResource.java
+++ src/main/java/com/hbhb/cw/systemcenter/model/SysResource.java
@@ -1,17 +1,13 @@
 package com.hbhb.cw.systemcenter.model;
 
 import org.beetl.sql.annotation.entity.AutoID;
-import org.beetl.sql.fetch.annotation.Fetch;
-import org.beetl.sql.fetch.annotation.FetchSql;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 import lombok.Data;
 
 @Data
-@Fetch
 public class SysResource implements Serializable {
     private static final long serialVersionUID = -6628686180184947562L;
     @AutoID
@@ -21,9 +17,4 @@ public class SysResource implements Serializable {
     private String apiPath;
     private String desc;
     private Date createTime;
-    /**
-     * todo 优化
-     */
-    @FetchSql("select role_id from sys_role_resource where resource_id = #{id}")
-    private List<String> roleIds;
 }
