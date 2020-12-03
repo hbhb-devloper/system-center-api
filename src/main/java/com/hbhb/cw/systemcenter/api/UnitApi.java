6996f87fd263dd08d44e65eecede01fa169473d8
xiaokang <648638730@qq.com>
2020-12-03 14:57:51 +0800

    代码优化

--- src/main/java/com/hbhb/cw/systemcenter/api/UnitApi.java
+++ src/main/java/com/hbhb/cw/systemcenter/api/UnitApi.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UnitApi {
 
     @GetMapping("/top")
     UnitTopVO getTopUnit();
 
-    @GetMapping("/all")
-    List<Unit> getAllUnit();
+    @GetMapping("/map/unit-name")
+    Map<Integer, String> getUnitMapByName();
+
+    @GetMapping("/map/abbr")
+    Map<String, Integer> getUnitMapByAbbr();
 
     @GetMapping("/{unitId}")
     Unit getUnitInfo(@PathVariable("unitId") Integer unitId);
 
-    @GetMapping("/sub/{unitId}")
-    List<Integer> getSubUnitId(@PathVariable("unitId") Integer unitId);
+    @GetMapping("/{unitId}/sub")
+    List<Integer> getSubUnit(@PathVariable("unitId") Integer unitId);
 }
--- src/main/java/com/hbhb/cw/systemcenter/model/Unit.java
+++ src/main/java/com/hbhb/cw/systemcenter/model/Unit.java
@@ -1,11 +1,14 @@
 package com.hbhb.cw.systemcenter.model;
 
-import lombok.Data;
+import org.beetl.sql.annotation.entity.AutoID;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 /**
  * @author dxk
@@ -14,49 +17,37 @@ import java.util.List;
 public class Unit implements Serializable {
     private static final long serialVersionUID = 7210208777877844759L;
 
+    @AutoID
     private Integer id;
-
-
+    @Schema(description = "单位名称")
     private String unitName;
-
-
+    @Schema(description = "上级单位id")
     private Integer parentId;
-
-
+    @Schema(description = "单位负责人")
     private String unitHeader;
-
-
+    @Schema(description = "单位级别")
     private String unitGrade;
-
+    @Schema(description = "单位职能")
     private String unitFunc;
-
-
+    @Schema(description = "单位电话")
     private String officePhone;
-
-
+    @Schema(description = "传真号")
     private String fax;
-
-
+    @Schema(description = "通信地址")
     private String address;
-
-
+    @Schema(description = "邮编")
     private String zipCode;
-
-
+    @Schema(description = "电子邮箱")
     private String email;
-
-
+    @Schema(description = "简称标志")
     private String abbr;
-
-
+    @Schema(description = "单位缩写")
     private String shortName;
-
+    @Schema(description = "创建时间")
     private Date createTime;
-
+    @Schema(description = "更新时间")
     private Date updateTime;
-
-
+    @Schema(description = "显示顺序")
     private Integer sortNum;
-
     private List<Unit> children = new ArrayList<>();
 }
