b0ffbdb201c4641e8bcffcedb45f572cf80c0b10
chenrim <1033258858@qq.com>
2021-01-14 16:47:51 +0800

    //加入builder

--- src/main/java/com/hbhb/cw/systemcenter/model/SysUserUintHall.java
+++ src/main/java/com/hbhb/cw/systemcenter/model/SysUserUintHall.java
@@ -1,12 +1,14 @@
 package com.hbhb.cw.systemcenter.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SysUserUintHall implements Serializable {
