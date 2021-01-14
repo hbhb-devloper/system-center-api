f0943c3fb3bd4a1734768af13d18a96df108a71c
chenrim <1033258858@qq.com>
2021-01-14 16:28:02 +0800

    //更新user单位营业厅关联实体类

---
+++ src/main/java/com/hbhb/cw/systemcenter/model/SysUserUintHall.java
@@ -0,0 +1,33 @@
+package com.hbhb.cw.systemcenter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SysUserUintHall implements Serializable {
+
+    private static final long serialVersionUID = 5767344039315276786L;
+
+    /**
+     * 主键ID
+     */
+    private Integer id;
+    /**
+     * 用户ID
+     */
+    private Integer userId;
+    /**
+     * 菜单ID
+     */
+    private Integer uintId;
+    /**
+     * 营业厅ID
+     */
+    private Integer hallId;
+
+}
