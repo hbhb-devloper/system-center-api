74392d8e2679b9ccc7cca76c9b5f5941b3002e5f
chenrim <1033258858@qq.com>
2021-01-14 16:22:29 +0800

    //更新已选择的营业厅(前端提交已选择id)

--- src/main/java/com/hbhb/cw/systemcenter/vo/HallSelectReqVO.java
+++ src/main/java/com/hbhb/cw/systemcenter/vo/HallSelectReqVO.java
@@ -1,6 +1,7 @@
 package com.hbhb.cw.systemcenter.vo;
 
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +17,7 @@ public class HallSelectReqVO implements Serializable {
     private static final long serialVersionUID = -7613178516718076881L;
 
     @Schema(description = "已选择的营业厅")
-    private List<HallReqVO> hallReqVOS;
+    private List<Integer> hallSelectIds;
     @Schema(description = "所属分公司id")
     private Integer unitId;
 
